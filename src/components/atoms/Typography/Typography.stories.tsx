c1448986c5440db92305abb41cc533fc6ae9d70f
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-03-07 22:44:00 -0300

    Added fontSize and fontWeight to the Typography

--- src/components/atoms/Typography/Typography.stories.tsx
+++ src/components/atoms/Typography/Typography.stories.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Typography } from './Typography';
 
 export default {
-  title: 'Typography'
+  title: 'atoms/Typography'
 }
 
 export const Default = () => <Typography>WELCOME</Typography>;
-export const DefaultPrimary = () => <Typography color='primary'>WELCOME</Typography>;
 export const Heading = () => <Typography variant='heading'>USER AREA</Typography>;
 export const Subheading = () => <Typography variant='subheading'>MY INTERESTS</Typography>;
+export const ContentPrimary = () => <Typography variant='content' color='primary'>USERNAME</Typography>;
+export const ContentBold = () => <Typography variant='content' weight='bold'>USERNAME</Typography>;
+export const ContentLarge = () => <Typography variant='content' size='large'>USERNAME</Typography>;
--- src/components/atoms/Typography/Typography.tsx
+++ src/components/atoms/Typography/Typography.tsx
@@ -4,25 +4,29 @@ import styled from 'styled-components';
 type TypographyProps = {
   variant?: 'heading' | 'subheading' | 'content';
   color?: 'text' | 'primary';
+  size?: 'small' | 'large';
+  weight?: 'normal' | 'bold';
 };
 
 const StyledHeading = styled.h1<TypographyProps>`
   font-family: ${(props) => props.theme.main.fontFamily};
   color: ${(props) => props.theme.main.colors[props.color ? props.color : 'text']};
-  font-size: 24px;
-  font-weight: 400;
+  font-size: ${(props) => props.theme.main.fontSizes.large};
+  font-weight: ${(props) => props.theme.main.fontWeights.normal};
 `;
 
 const StyledSubheading = styled.h2<TypographyProps>`
   font-family: ${(props) => props.theme.main.fontFamily};
   color: ${(props) => props.theme.main.colors[props.color ? props.color : 'text']};
-  font-size: 14px;
-  font-weight: 400;
+  font-size: ${(props) => props.theme.main.fontSizes.small};
+  font-weight: ${(props) => props.theme.main.fontWeights.normal};
 `;
 
 const StyledContent = styled.span<TypographyProps>`
   font-family: ${(props) => props.theme.main.fontFamily};
   color: ${(props) => props.theme.main.colors[props.color ? props.color : 'text']};
+  font-size: ${(props) => props.theme.main.fontSizes[props.size ? props.size : 'small']};
+  font-weight: ${(props) => props.theme.main.fontWeights[props.weight ? props.weight : 'normal']};
 `;
 
 export const Typography: FC<TypographyProps> = ({ children, variant, ...props }) => {
