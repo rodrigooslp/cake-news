d05d26bca63991d25a6786858634dc756ba417ea
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-03-07 22:56:52 -0300

    Added the MenuHandler organism
    It uses Hamburger atom and Menu molecule to implement the menu actions.

---
+++ src/components/organisms/MenuHandler/MenuHandler.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { Hamburger } from 'components/atoms';
+import { Menu } from 'components/molecules';
+
+type MenuHandlerProps = {
+  handleMenuClick: (slug: string) => void;
+};
+
+type MenuContainerProps = {
+  open: boolean;
+};
+
+const StyledMenuHandler = styled.div`
+  background-color: ${(props) => props.theme.main.colors.white};
+`;
+
+const HamburgerContainer = styled.div`
+  position: absolute;
+  left: 52px;
+`;
+
+const MenuContainer = styled.div<MenuContainerProps>`
+  position: absolute;
+  top: 57px;
+  left: 0;
+  width: 100%;
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(-100%);
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+`;
+
+export const MenuHandler: FC<MenuHandlerProps> = ({ handleMenuClick, ...props }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  const handleClickOutside = (event: any) => {
+    if (!ref) return;
+    if (!ref.current) return;
+    if (!ref.current.contains(event.target)) setOpenMenu(false);
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+
+  return (
+    <StyledMenuHandler ref={ref}>
+      <HamburgerContainer>
+        <Hamburger onClick={() => setOpenMenu(!openMenu)}/>
+      </HamburgerContainer>
+
+      <MenuContainer open={openMenu}>
+        <Menu handleMenuClick={handleMenuClick}/>
+      </MenuContainer>
+    </StyledMenuHandler>
+  );
+};
