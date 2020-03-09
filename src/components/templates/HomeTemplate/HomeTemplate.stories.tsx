1999ec6d5cde2f3c13a3114f579eb0c4a9c6408a
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-03-09 04:08:15 -0300

    Added the Home template with all cards and layouts

--- src/components/templates/HomeTemplate/HomeTemplate.stories.tsx
+++ src/components/templates/HomeTemplate/HomeTemplate.stories.tsx
@@ -1,13 +1,65 @@
 import { action } from '@storybook/addon-actions';
 import React from 'react';
 import { HomeTemplate } from './HomeTemplate';
+import { News } from 'models';
 
 export default {
   title: 'templates/HomeTemplate'
 }
 
+const news: News[] = [{
+  id: 1,
+  type: 'politics',
+  image: 'news-1',
+  title: 'Obama Offers Hopeful Vision While Noting Nation’s Fears',
+  avatar: 'https://i.pravatar.cc/45?u=pambeesly@dundermifflin.com',
+  author: 'Pam Beesly',
+  content: 'In his last State of the Union address, President Obama sought to paint a hopeful portrait. But he acknowledged that many Americans felt shut out of a political and economic system they view as rigged.'
+}, {
+  id: 2,
+  type: 'tech',
+  image: 'news-2',
+  title: 'Didi Kuaidi, The Company Beating Uber In China, Opens Its API To Third Party Apps',
+  avatar: 'https://i.pravatar.cc/45?u=thats@verylongname.com',
+  author: 'Pam Beesly',
+  content: 'One day after Uber updated its API to add ‘content experiences’ for passengers, the U.S. company’s biggest rival — Didi Kuaidi in China — has opened its own platform up by releasing an SDK for developers and third-parties.'
+}, {
+  id: 3,
+  type: 'science',
+  image: 'news-3',
+  title: 'NASA Formalizes Efforts To Protect Earth From Asteroids',
+  avatar: 'https://i.pravatar.cc/45?u=pambeesly@dundermifflin.com',
+  author: 'Alexandre Henrique Shailesh Zeta-Jones',
+  content: 'Last week, NASA announced a new program called the Planetary Defense Coordination Office (PDCO) which will coordinate NASA’s efforts for detecting and tracking near-Earth objects (NEOs). If a large object comes hurtling toward our planet… '
+}, {
+  id: 4,
+  type: 'sports',
+  title: 'For Some Atlanta Hawks, a Revved-Up Game of Uno Is Diversion No. 1',
+  avatar: 'https://i.pravatar.cc/45?u=pambeesly@dundermifflin.com',
+  author: 'Pam Beesly',
+  content: 'The favored in-flight pastime of a group of players including Al Horford, Kent Bazemore and Dennis Schroder is a schoolchildren’s card game with some added twists.'
+}, {
+  id: 5,
+  type: 'tech',
+  title: 'Picking a Windows 10 Security Package',
+  avatar: 'https://i.pravatar.cc/45?u=pambeesly@dundermifflin.com',
+  author: 'Pam Beesly',
+  content: 'Oscar the Grouch has a recycling bin and Big Bird has moved to a tree as the children’s classic debuts on HBO, aiming at a generation that doesn’t distinguish between TV and mobile screens.'
+}, {
+  id: 6,
+  type: 'science',
+  title: 'As U.S. Modernizes Nuclear Weapons, ‘Smaller’ Leaves Some Uneasy',
+  avatar: 'https://i.pravatar.cc/45?u=pambeesly@dundermifflin.com',
+  author: 'Pam Beesly',
+  content: 'The Energy Department and the Pentagon have been readying a weapon with a build-it-smaller approach, setting off a philosophical clash in the world of nuclear arms.'
+}];
+
 export const Default = () =>
   <HomeTemplate
+    news={news}
+    handleTagClick={action('clicked tag')}
+    handleCardClick={action('clicked card')}
+    handleAuthorClick={action('clicked author')}
     handleLogoClick={action('clicked logo')}
     handleMenuClick={action('clicked menu')}
   />;
@@ -15,6 +67,10 @@ export const Default = () =>
   export const LoggedIn = () =>
   <HomeTemplate
     isLoggedIn
+    news={news}
+    handleTagClick={action('clicked tag')}
+    handleCardClick={action('clicked card')}
+    handleAuthorClick={action('clicked author')}
     handleLogoClick={action('clicked logo')}
     handleMenuClick={action('clicked menu')}
   />;
--- src/components/templates/HomeTemplate/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate/HomeTemplate.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
+import { Row, Col } from 'react-awesome-styled-grid'
 import styled from 'styled-components';
-import { AppBar, DesktopAppBar } from 'components/organisms'
+import { AppBar, DesktopAppBar, Card } from 'components/organisms'
+import { News } from 'models';
 
 type HomeTemplateProps = {
+  news: News[];
   isLoggedIn?: boolean;
   handleMenuClick: (slug: string) => void;
   handleLogoClick: () => void;
+  handleAuthorClick: (author: string) => void;
+  handleTagClick: (slug: string) => void;
+  handleCardClick: (id: number) => void;
 };
 
 const StyledHomeTemplate = styled.div`
@@ -26,11 +32,70 @@ const StyleDesktopAppBar = styled(DesktopAppBar)`
   ${desktop(`display: flex;`)};
 `;
 
-export const HomeTemplate: FC<HomeTemplateProps> = ({ handleLogoClick, handleMenuClick, isLoggedIn, ...props }) => {
+const StyledContainer = styled.div`
+  margin: 0 15px;
+
+  @media only screen and (min-width: 768px) {
+    margin: 0 40px;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    margin: 0 130px;
+  }
+`;
+
+const Separator = styled.div`
+  width: 100%;
+  border: 1px solid #E4E4E4;
+  ${mobile(`display: none;`)};
+  ${desktop(`display: flex;`)};
+`;
+
+export const HomeTemplate: FC<HomeTemplateProps> = ({ news, handleLogoClick, handleMenuClick, handleAuthorClick, handleTagClick, handleCardClick, isLoggedIn, ...props }) => {
+  const [featuredNews, secondNews, thirdNews, ...listNews] = news;
+
+  const setHandlers = (item: News) => {
+    return {
+      handleTagClick,
+      handleAuthorClick: () => handleAuthorClick(item.author),
+      handleTitleClick: () => handleCardClick(item.id),
+      handleCardContentClick: () => handleCardClick(item.id),
+      handleReadMoreClick: () => handleCardClick(item.id)
+    }
+  };
+
   return (
     <StyledHomeTemplate { ...props }>
       <StyledAppBar handleLogoClick={handleLogoClick} isLoggedIn={isLoggedIn} handleMenuClick={handleMenuClick} />
       <StyleDesktopAppBar handleLogoClick={handleLogoClick} isLoggedIn={isLoggedIn} handleMenuClick={handleMenuClick} />
+
+      <StyledContainer>
+        <Row>
+          <Col xs={12} sm={12} md={6}>
+            <Card featured {...featuredNews} {...setHandlers(featuredNews)} />
+          </Col>
+          <Col xs={12} sm={6} md={3}>
+            <Card {...secondNews}  {...setHandlers(secondNews)} />
+          </Col>
+          <Col xs={12} sm={6} md={3}>
+            <Card {...thirdNews}  {...setHandlers(thirdNews)} />
+          </Col>
+        </Row>
+
+        <Row>
+          <Separator></Separator>
+        </Row>
+
+        <Row>
+          {
+            listNews.map(({ image, ...item }) =>
+              <Col xs={12} sm={6} md={4}>
+                <Card {...item} {...setHandlers(item)} />
+              </Col>
+            )
+          }
+        </Row>
+      </StyledContainer>
     </StyledHomeTemplate>
   );
 };
